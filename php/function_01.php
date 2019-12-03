6180421ce80ae3923df3b2b3cc5bfa7fb169211a
남기열 <50548404+skarlduf@users.noreply.github.com>
2019-12-04 02:57:19 +0900

    test

---
+++ php/function_01.php
@@ -0,0 +1,29 @@
+<?php
+
+$name = "기열이"; 
+
+// 호출
+
+
+// 선언
+if(!function_exists("hello")) {
+    function hello()
+    {
+        echo "반가와요 "; 
+    }
+}
+
+
+// 선언
+if(!function_exists("hello")) {
+    function hello()
+    {
+        echo "그만해요 "; 
+    }
+}
+
+hello();
+hello();
+
+
+
